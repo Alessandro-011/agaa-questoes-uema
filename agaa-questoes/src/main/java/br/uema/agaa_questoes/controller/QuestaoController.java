c83eee5a62b17cae27cbef6cc48e76f558734eb4
Afonso Gabriel <agfbezerra@gmail.com>
2026-03-03 00:50:24 -0300

    feat(questao-controller): adiciona validação com @Valid e handler de erros

--- agaa-questoes/src/main/java/br/uema/agaa_questoes/controller/QuestaoController.java
+++ agaa-questoes/src/main/java/br/uema/agaa_questoes/controller/QuestaoController.java
@@ -2,9 +2,15 @@ package br.uema.agaa_questoes.controller;
 
 import br.uema.agaa_questoes.model.Questao;
 import br.uema.agaa_questoes.repository.QuestaoRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/questoes")
@@ -17,13 +23,23 @@ public class QuestaoController {
     }
 
     @GetMapping
-    public List<Questao> findAll(){
+    public List<Questao> findAll() {
         return repository.findAll();
     }
 
     @PostMapping
-    public Questao save(@RequestBody Questao questao){
+    public Questao save(@Valid @RequestBody Questao questao) {
         return repository.save(questao);
     }
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return errors;
+    }
 }
